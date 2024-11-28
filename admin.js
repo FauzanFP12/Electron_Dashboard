57d62c95ad25d2870761a081a88875d005ffa6c6
Fauzan Fuadi Pasaribu <120654283+FauzanFP12@users.noreply.github.com>
2024-11-28 17:01:22 +0700

    Add files via upload

---
+++ admin.js
@@ -0,0 +1,34 @@
+import bcrypt from 'bcryptjs';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import User from '../models/User.js'; // Adjust path if necessary
+
+dotenv.config(); // Load environment variables
+
+// Check that the MongoDB URI is loaded
+if (!process.env.MONGO_URI) {
+  console.error("MongoDB URI not found in environment variables.");
+  process.exit(1);
+}
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(async () => {
+    const hashedPassword = bcrypt.hashSync('adminpassword', 10);
+    const newUser = new User({
+      username: 'admin',
+      password: hashedPassword,
+      role: 'admin',
+      fullName: 'Admin',
+    });
+    await newUser.save();
+    console.log('User created successfully');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  })
+  .finally(() => {
+    mongoose.connection.close(); // Close the connection regardless of outcome
+  });
