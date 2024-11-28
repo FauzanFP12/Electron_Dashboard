c9119b17acbc1afdb4980d9fd32f5adf78bfd283
Fauzan Fuadi Pasaribu <120654283+FauzanFP12@users.noreply.github.com>
2024-11-28 17:29:57 +0700

    Update server.js

--- win-unpacked/resources/app/dashboard/backend/server.js
+++ win-unpacked/resources/app/dashboard/backend/server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || 'http://10.255.254.189:3000',
+    origin: process.env.FRONTEND_URL || 'http://10.255.254.166:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
   },
